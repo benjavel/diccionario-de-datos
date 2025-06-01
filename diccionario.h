1e075cb160e0ba27f8ac145b3908d042244e9622
benjavel <benjaminvelazquez104@gmail.com>
2025-06-01 18:17:58 -0300

    Fixes menores

---
+++ diccionario.h
@@ -0,0 +1,69 @@
+#ifndef DICCIONARIO_H_INCLUDED
+#define DICCIONARIO_H_INCLUDED
+
+#include "lista.h"
+#include <stdlib.h>
+
+#define TODO_OK 1
+#define ERROR 0
+
+/* typedef struct s_nodo {
+    void* clave, *valor;
+    size_t tamClave, tamValor;
+    struct s_nodo* sig;
+} t_nodo; */
+
+// typedef struct
+// {
+//     void* clave, *valor;
+//     size_t tamClave, tamValor;
+// }t_clave_valor;
+
+typedef struct
+{
+    void* clave, *valor;
+    size_t tamClave, tamValor;
+}t_clave_valor;
+
+
+typedef struct {
+    t_lista *buckets; // vector de listas
+    size_t capacidad;
+    unsigned long (*hash)(void*);
+    int (*cmp)(const void *e1, const void *e2);
+} t_diccionario;
+
+// /**
+// debe recibir por par�metro una capacidad m�xima y definir los valores
+// necesarios para inicializar el diccionario.
+// */
+int crear_dic(t_diccionario *pdic, size_t capacidad, unsigned long (*hash)(void *), int (*cmp)(const void *e1, const void *e2));
+// /**
+// agrega un nuevo elemento al diccionario. Recibe la clave y el valor a
+// agregar al diccionario, calcula la posici�n correcta para insertarlo y maneja las
+// colisiones.
+// */
+// int poner_dic(t_diccionario * pdic, void* clave, void *valor, size_t tamClave, size_t tamValor, void (*acumulador)(void*, void*));
+
+// /**
+// busca un elemento en el diccionario por clave y devuelve el valor.
+// */
+// //int obtener_dic();
+
+// /**
+// Elimina un elemento del diccionario buscando por clave.
+// */
+// //int sacar_dic();
+
+// /**
+// Recorre el diccionario de datos ejecutando una acci�n para cada
+// elemento presente.
+// */
+// int recorrer_dic(t_diccionario* pdic, void (*accion)(void*, void*), void* params);
+
+// /**
+// Vac�a el diccionario y libera toda la memoria
+// */
+// int vaciar_dic();
+
+#endif // DICCIONARIO_H_INCLUDED
--- main.c
+++ main.c
@@ -4,5 +4,22 @@
 int main()
 {
     printf("Diccionario de datos:");
+<<<<<<< Updated upstream
+=======
+    t_diccionario dic;
+    crear_dic(&dic, 1000, hash_DJB2a, comparar_string);
+
+    char* clave = "CLAVE";
+    char* valor = "valor";
+    poner_dic(&dic, clave, valor, 6, 6, NULL);
+
+    t_clave_valor cv;
+    cv.clave = clave;
+    cv.tamClave = 6;
+    sacar_dic(&dic, &cv);
+
+    printf("\nClave: %s, Valor: %s", cv.clave, cv.valor);
+
+>>>>>>> Stashed changes
     return 0;
 }
